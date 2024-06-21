a2689ad352749f965f1023035353e7480d300ff1
Vladimir <vombat007@gmail.com>
2024-06-21 12:26:26 +0200

    update and add custom sidebar buttons

--- kivy_app/screens/py_files/home_screen.py
+++ kivy_app/screens/py_files/home_screen.py
@@ -1,5 +1,24 @@
+from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
+from kivy.animation import Animation
+
+
+class CustomSidebarButton(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.is_sidebar_open = False
+        self.update_style()
+
+    def toggle(self):
+        self.is_sidebar_open = not self.is_sidebar_open
+        self.update_style()
+
+    def update_style(self):
+        if self.is_sidebar_open:
+            self.background_normal = 'kivy_app/assets/img/return_button.png'
+        else:
+            self.background_normal = 'kivy_app/assets/img/settings_button.png'
 
 
 class HomeScreen(Screen):
@@ -20,4 +39,11 @@ class HomeScreen(Screen):
         self.ids.income_label.text = f'Income: ${income:.2f}'
 
     def toggle_sidebar(self):
-        self.sidebar.toggle_sidebar()
+        button = self.ids.sidebar_toggle_button
+        button.toggle()
+
+        if self.sidebar.x < 0:
+            anim = Animation(x=0, duration=0.3)
+        else:
+            anim = Animation(x=-self.sidebar.width, duration=0.3)
+        anim.start(self.sidebar)
