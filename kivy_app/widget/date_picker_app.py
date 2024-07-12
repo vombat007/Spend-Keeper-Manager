9241be85b8bdde356357952f4313472aa74631c4
Vladimir <vombat007@gmail.com>
2024-07-13 01:59:11 +0200

    Update custom date picker separate design to .kv file

--- kivy_app/widget/date_picker_app.py
+++ kivy_app/widget/date_picker_app.py
@@ -1,11 +1,11 @@
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import StringProperty
 from kivy.clock import Clock
 from datetime import datetime, timedelta
+from kivy.lang import Builder
+from kivy.uix.label import Label
 
 Builder.load_file('date_picker_app.kv')
 
@@ -25,24 +25,15 @@ class DatePicker(FloatLayout):
 
     def update_header(self):
         self.ids.header.clear_widgets()
-
-        prev_button = Button(size_hint=(None, None), size=(45, 32),
-                             background_normal='Button_arrow_left_normal.png',
-                             background_down='Button_arrow_left_down.png'
-                             )
+        prev_button = self.ids.prev_button
         prev_button.bind(on_press=self.prev_month)
         self.ids.header.add_widget(prev_button)
 
-        self.month_year_label = Label(text=self.selected_date.strftime('%B %Y'),
-                                      size_hint=(None, None),
-                                      size=(200, 30),
-                                      color=(0, 0, 0, 1))
+        self.month_year_label = self.ids.month_year_label
+        self.month_year_label.text = self.selected_date.strftime('%B %Y')
         self.ids.header.add_widget(self.month_year_label)
 
-        next_button = Button(size_hint=(None, None), size=(45, 32),
-                             background_normal='Button_arrow_right_normal.png',
-                             background_down='Button_arrow_right_down.png'
-                             )
+        next_button = self.ids.next_button
         next_button.bind(on_press=self.next_month)
         self.ids.header.add_widget(next_button)
 
@@ -67,37 +58,27 @@ class DatePicker(FloatLayout):
         for i in range(1, days_in_month + 1):
             date = self.selected_date.replace(day=i)
             if self.start_date and self.end_date and self.start_date <= date <= self.end_date:
-                day_button = Button(text=str(i), size_hint=(None, None), size=(40, 40),
-                                    background_normal='Yellow_circle.png',
-                                    background_down='',
-                                    color=(0, 0, 0, 1))
+                day_button = self.create_day_button(str(i), 'Yellow_circle.png')
 
             elif date == self.start_date or date == self.end_date:
-                day_button = Button(text=str(i), size_hint=(None, None), size=(40, 40),
-                                    background_normal='Yellow_circle.png',
-                                    background_down='',
-                                    color=(0, 0, 0, 1))
+                day_button = self.create_day_button(str(i), 'Yellow_circle.png')
 
             elif date.day == current_day and date.month == datetime.now().month and date.year == datetime.now().year:
-                day_button = Button(text=str(i), size_hint=(None, None), size=(40, 40),
-                                    background_normal='Red_circle.png',
-                                    background_down='',
-                                    color=(0, 0, 0, 1))  # Highlight current day
+                day_button = self.create_day_button(str(i), 'Red_circle.png')
 
             elif date < datetime.now():
-                day_button = Button(text=str(i), size_hint=(None, None), size=(40, 40),
-                                    background_normal='',
-                                    background_down='',
-                                    color=(0.7, 0.7, 0.7, 1))  # Pale color for past days
+                day_button = self.create_day_button(str(i), '', (0.7, 0.7, 0.7, 1))
             else:
-                day_button = Button(text=str(i), size_hint=(None, None), size=(40, 40),
-                                    background_normal='',
-                                    background_down='',
-                                    background_color=(1, 1, 1, 1),
-                                    color=(0, 0, 0, 1))
+                day_button = self.create_day_button(str(i))
 
             day_button.bind(on_press=self.select_date)
             self.ids.body.add_widget(day_button)
+
+    def create_day_button(self, text, background_normal='', color=(0, 0, 0, 1)):
+        return Button(text=text, size_hint=(None, None), size=(40, 40),
+                      background_normal=background_normal,
+                      background_down='',
+                      color=color)
 
     def prev_month(self, instance):
         Clock.schedule_once(self._prev_month_callback, 0.1)
@@ -135,7 +116,6 @@ class DatePicker(FloatLayout):
         self.update_body()
 
     def cancel(self, instance):
-        # Logic to cancel the date selection
         print("Date selection cancelled.")
         self.start_date = None
         self.end_date = None
@@ -148,8 +128,6 @@ class DatePicker(FloatLayout):
             print(f"Start date: {self.start_date.strftime('%Y-%m-%d')}")
         else:
             print("No date selected.")
-        # Logic to confirm the date selection
-        # Reset selection after confirming
         self.start_date = None
         self.end_date = None
         self.update_body()
@@ -157,10 +135,7 @@ class DatePicker(FloatLayout):
 
 class DatePickerApp(App):
     def build(self):
-        root = FloatLayout()
-        date_picker = DatePicker(size_hint=(None, None), size=(350, 400), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        root.add_widget(date_picker)
-        return root
+        return DatePicker()
 
 
 if __name__ == '__main__':
