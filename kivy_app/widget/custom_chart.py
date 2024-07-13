ac34587ff2030d1c7bcfaa81e7a3e487da87138e
Vladimir <vombat007@gmail.com>
2024-07-13 14:58:11 +0200

    change size custom_chart.py in home screen

--- kivy_app/widget/custom_chart.py
+++ kivy_app/widget/custom_chart.py
@@ -71,7 +71,7 @@ class CircularChart(Widget):
             # Draw the total balance in the center
             self.draw_text(f'Balance $ {self.total_balance}', self.center_x, self.center_y - 10)
 
-            self.draw_text(f'Spend % {self.percent_spent}', self.center_x, self.center_y - 50)
+            self.draw_text(f'Spend {self.percent_spent}%', self.center_x, self.center_y - 50)
 
     def draw_text(self, text, x, y):
         font_path = 'kivy_app/assets/fonts/IrishGrover-Regular.ttf'  # Specify the path to your custom font
