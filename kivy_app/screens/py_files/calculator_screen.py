792119ae8e2fa81f5b58843ced9b04614636ac1c
Vladimir <vombat007@gmail.com>
2024-08-01 18:09:44 +0200

    Update calculator to when button ok click send result ot transactions screen

--- kivy_app/screens/py_files/calculator_screen.py
+++ kivy_app/screens/py_files/calculator_screen.py
@@ -55,10 +55,10 @@ class CalculatorScreen(Screen):
 
     def calculate(self):
         try:
-            # Protect against dangerous eval input
             self.result_text = str(eval(self.operation_text))
-            self.ids.equals_button.text = "OK"  # Change the equals button text to OK
-            self.ids.operation.height = 0  # Hide the operation display
+            self.ids.equals_button.text = "OK"
+            self.ids.equals_button.bind(on_press=self.on_ok)  # Bind the on_ok method to the OK button
+            self.ids.operation.height = 0
             self.ids.result.font_size = 100
             self.ids.result.height = '150dp'
         except Exception:
@@ -70,11 +70,15 @@ class CalculatorScreen(Screen):
         except Exception:
             self.result_text = ""
 
-    def on_ok(self):
+    def on_ok(self, instance):
+        # Set the amount_input text on the transaction screen to the result
+        self.manager.get_screen('transaction').ids.amount_input.text = self.result_text
         self.reset_calculator()
+        self.manager.current = 'transaction'
 
     def reset_calculator(self):
-        self.ids.equals_button.text = "="  # Reset the button text to equals
+        self.ids.equals_button.text = "="
+        self.ids.equals_button.unbind(on_press=self.on_ok)  # Unbind the on_ok method from the OK button
         self.operation_text = ""
         self.result_text = "0"
         self.ids.operation.height = '50dp'
