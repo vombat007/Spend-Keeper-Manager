ccd0990289b90b4c49e4acb53842f7e7f2fbcec6
Vladimir <vombat007@gmail.com>
2024-08-11 14:58:48 +0200

    Add icon

--- kivy_app/screens/py_files/create_category_screen.py
+++ kivy_app/screens/py_files/create_category_screen.py
@@ -5,7 +5,6 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty, ListProperty
 from kivy.graphics import Color, RoundedRectangle
