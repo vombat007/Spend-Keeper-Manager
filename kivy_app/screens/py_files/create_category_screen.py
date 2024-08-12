6495540a51671128acdc3040214a83a149f4f2ed
Vladimir <vombat007@gmail.com>
2024-08-12 22:42:11 +0200

    update color picker

--- kivy_app/screens/py_files/create_category_screen.py
+++ kivy_app/screens/py_files/create_category_screen.py
@@ -84,44 +84,95 @@ class CreateCategoryScreen(Screen):
         color_grid.clear_widgets()
 
         color_choices = [
-            [0.753, 0.047, 0.047, 1.0],
-            [0.753, 0.059, 0.047, 1.0],
-            [0.925, 0.133, 0.122, 1.0],
-            [0.871, 0.29, 0.29, 1.0],
-            [0.831, 0.176, 0.176, 1.0],
-            [0.965, 0.231, 0.231, 1.0],
-            [0.949, 0.392, 0.392, 1.0],
-            [1.0, 0.0, 0.0, 1.0],
-            [0.894, 0.098, 0.098, 1.0],
-            [1.0, 0.216, 0.216, 1.0],
-            [1.0, 0.251, 0.251, 1.0],
-            [0, 1, 0, 1],
-            [0, 0, 1, 1],
-            [1, 1, 0, 1],
-            [1, 0.5, 0, 1],
-            [0.5, 0, 0.5, 1],
-            [0, 1, 1, 1],
-            [1, 0, 1, 1],
-            [0.5, 0.5, 0.5, 1],
-            [0, 0, 0, 1],
-            [0, 1, 0, 1],
-            [0, 0, 1, 1],
-            [1, 1, 0, 1],
-            [1, 0.5, 0, 1],
-            [0.5, 0, 0.5, 1],
-            [0, 1, 1, 1],
-            [1, 0, 1, 1],
-            [0.5, 0.5, 0.5, 1],
-            [0, 0, 0, 1],
-            [0, 1, 0, 1],
-            [0, 0, 1, 1],
-            [1, 1, 0, 1],
-            [1, 0.5, 0, 1],
-            [0.5, 0, 0.5, 1],
-            [0, 1, 1, 1],
-            [1, 0, 1, 1],
-            [0.5, 0.5, 0.5, 1],
-            [0, 0, 0, 1],
+            # Red gradient
+            [1.0, 0.0, 0.0, 1.0],  # Bright Red
+            [0.9, 0.0, 0.0, 1.0],
+            [0.8, 0.0, 0.0, 1.0],
+            [0.7, 0.0, 0.0, 1.0],
+            [0.6, 0.0, 0.0, 1.0],
+            [0.5, 0.0, 0.0, 1.0],
+            [0.4, 0.0, 0.0, 1.0],  # Pale Red
+
+            # Green gradient
+            [0.0, 1.0, 0.0, 1.0],  # Bright Green
+            [0.0, 0.9, 0.0, 1.0],
+            [0.0, 0.8, 0.0, 1.0],
+            [0.0, 0.7, 0.0, 1.0],
+            [0.0, 0.6, 0.0, 1.0],
+            [0.0, 0.5, 0.0, 1.0],
+            [0.0, 0.4, 0.0, 1.0],  # Pale Green
+
+            # Blue gradient
+            [0.0, 0.0, 1.0, 1.0],  # Bright Blue
+            [0.0, 0.0, 0.9, 1.0],
+            [0.0, 0.0, 0.8, 1.0],
+            [0.0, 0.0, 0.7, 1.0],
+            [0.0, 0.0, 0.6, 1.0],
+            [0.0, 0.0, 0.5, 1.0],
+            [0.0, 0.0, 0.4, 1.0],  # Pale Blue
+
+            # Yellow gradient
+            [1.0, 1.0, 0.0, 1.0],  # Bright Yellow
+            [0.9, 0.9, 0.0, 1.0],
+            [0.8, 0.8, 0.0, 1.0],
+            [0.7, 0.7, 0.0, 1.0],
+            [0.6, 0.6, 0.0, 1.0],
+            [0.5, 0.5, 0.0, 1.0],
+            [0.4, 0.4, 0.0, 1.0],  # Pale Yellow
+
+            # Orange gradient
+            [1.0, 0.5, 0.0, 1.0],  # Bright Orange
+            [0.9, 0.45, 0.0, 1.0],
+            [0.8, 0.4, 0.0, 1.0],
+            [0.7, 0.35, 0.0, 1.0],
+            [0.6, 0.3, 0.0, 1.0],
+            [0.5, 0.25, 0.0, 1.0],
+            [0.4, 0.2, 0.0, 1.0],  # Pale Orange
+
+            # Purple gradient
+            [0.5, 0.0, 0.5, 1.0],  # Bright Purple
+            [0.45, 0.0, 0.45, 1.0],
+            [0.4, 0.0, 0.4, 1.0],
+            [0.35, 0.0, 0.35, 1.0],
+            [0.3, 0.0, 0.3, 1.0],
+            [0.25, 0.0, 0.25, 1.0],
+            [0.2, 0.0, 0.2, 1.0],  # Pale Purple
+
+            # Cyan gradient
+            [0.0, 1.0, 1.0, 1.0],  # Bright Cyan
+            [0.0, 0.9, 0.9, 1.0],
+            [0.0, 0.8, 0.8, 1.0],
+            [0.0, 0.7, 0.7, 1.0],
+            [0.0, 0.6, 0.6, 1.0],
+            [0.0, 0.5, 0.5, 1.0],
+            [0.0, 0.4, 0.4, 1.0],  # Pale Cyan
+
+            # Magenta gradient
+            [1.0, 0.0, 1.0, 1.0],  # Bright Magenta
+            [0.9, 0.0, 0.9, 1.0],
+            [0.8, 0.0, 0.8, 1.0],
+            [0.7, 0.0, 0.7, 1.0],
+            [0.6, 0.0, 0.6, 1.0],
+            [0.5, 0.0, 0.5, 1.0],
+            [0.4, 0.0, 0.4, 1.0],  # Pale Magenta
+
+            # Brown gradient (replacing Black)
+            [0.6, 0.3, 0.0, 1.0],  # Bright Brown
+            [0.55, 0.275, 0.0, 1.0],
+            [0.5, 0.25, 0.0, 1.0],
+            [0.45, 0.225, 0.0, 1.0],
+            [0.4, 0.2, 0.0, 1.0],
+            [0.35, 0.175, 0.0, 1.0],
+            [0.3, 0.15, 0.0, 1.0],  # Pale Brown
+
+            # Pink gradient (added new color)
+            [1.0, 0.75, 0.8, 1.0],  # Bright Pink
+            [0.9, 0.675, 0.72, 1.0],
+            [0.8, 0.6, 0.64, 1.0],
+            [0.7, 0.525, 0.56, 1.0],
+            [0.6, 0.45, 0.48, 1.0],
+            [0.5, 0.375, 0.4, 1.0],
+            [0.4, 0.3, 0.32, 1.0],  # Pale Pink
         ]
 
         for color in color_choices:
@@ -158,23 +209,19 @@ class CreateCategoryScreen(Screen):
     def toggle_display(self, display_type):
         if display_type == 'icon':
             self.ids.icon_grid.height = self.ids.icon_grid.minimum_height
-            self.ids.icon_grid.size_hint_y = None
             self.ids.icon_grid.opacity = 1
             self.ids.icon_grid.disabled = False
 
             self.ids.color_grid.height = 0
-            self.ids.color_grid.size_hint_y = None
             self.ids.color_grid.opacity = 0
             self.ids.color_grid.disabled = True
 
         elif display_type == 'color':
             self.ids.icon_grid.height = 0
-            self.ids.icon_grid.size_hint_y = None
             self.ids.icon_grid.opacity = 0
             self.ids.icon_grid.disabled = True
 
-            self.ids.color_grid.height = dp(200)
-            self.ids.color_grid.size_hint_y = None
+            self.ids.color_grid.height = self.ids.color_grid.minimum_height
             self.ids.color_grid.opacity = 1
             self.ids.color_grid.disabled = False
 
