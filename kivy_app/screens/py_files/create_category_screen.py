d1df4f7b0b555ebdae5eec83f3d132d44677c919
Vladimir <vombat007@gmail.com>
2024-08-13 14:24:04 +0200

    fix track button state if user change button income or expense

--- kivy_app/screens/py_files/create_category_screen.py
+++ kivy_app/screens/py_files/create_category_screen.py
@@ -1,16 +1,15 @@
 import os
 from kivy.metrics import dp
-from kivy.uix.popup import Popup
-from kivy.uix.screenmanager import Screen
-from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.behaviors import ButtonBehavior
 from kivy.properties import StringProperty, ListProperty
 from kivy.graphics import Color, Ellipse, RoundedRectangle
-from kivy.uix.behaviors import ButtonBehavior
-from kivy.uix.widget import Widget
-from kivy.clock import Clock
 
 
 class ColorCircleButton(ButtonBehavior, Widget):
